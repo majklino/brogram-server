d1426702556b8664ad58cb60791e15e5978db49e
unknown <mcervinka17@gmail.com>
2023-09-30 13:52:59 +0200

    added bodyParser

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const config = require('config');
+const bodyParser = require('body-parser')
+
 const log = require('./helpers/logger');
 
 const app = express();
 const PORT = process.env.PORT || config.get('port');
+
+// Use body-parser middleware to parse POST request bodies
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 //Define routes
 defineRoutes = require('./routes/routes-handler');
