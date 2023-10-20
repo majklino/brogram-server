e4726494c44c25230faad71d0f23aacd7444b2f1
Červinka <mcervinka17@gmail.com>
2023-10-20 12:12:53 +0200

    added logout feature

--- model/master/sql-service.js
+++ model/master/sql-service.js
@@ -37,6 +37,19 @@ class SqlService{
         }
     }
 
+    async logoutUser(username, online_uuid){
+        let query = 'select id from users where username = ? and online_uuid = ?;';
+        let results = await this.handler.executeQuery(query, [username, online_uuid]);
+        if(results.length == 0){
+            return false;
+        }
+        else{
+            query = 'update users set online_uuid = NULL, last_online = NOW() where username = ? and online_uuid = ?;'
+            await this.handler.executeQuery(query, [username, online_uuid])
+            return true;
+        }
+    }
+
     async checkUserExistence(username){
         let query = 'select username from users where username = ?;';
         let results = await this.handler.executeQuery(query, [username]);
--- routes/login/login-routes.js
+++ routes/login/login-routes.js
@@ -24,10 +24,36 @@ router.post('/', async function(req, res) {
         let results = await sqlService.loginUser(username, password, uuid);
         await sqlService.disconnect();
         if(results == null){
-            res.json({success: {status: "WRONG_CREDENTIALS", message: "the username and/or password are incorrect!", data: results}});
+            res.json({error: {status: "WRONG_CREDENTIALS", message: "the username and/or password are incorrect!", data: results}});
         }
         else{
-        res.json({success: {status: "USER_LOGGED_IN", message: "the user has been successfully logged in", data: results}});
+            res.json({success: {status: "USER_LOGGED_IN", message: "the user has been successfully logged in", data: results}});
+        }
+    }
+    
+});
+
+router.post('/logout', async function(req, res) {
+    const requestData = req.body;
+    let username = requestData.username;
+    let online_uuid = requestData.online_uuid;
+
+    if(username == null || username == ''){
+        res.json({error: {status: "USER_NOT_SPECIFIED", message: "the request does not specify a username!"}});
+    }
+    else if(online_uuid == null || online_uuid == ''){
+        res.json({error: {status: "ONLINE_UUID_NOT_SPECIFIED", message: "the request does not specify an online uuid!"}});
+    }
+    else{
+        await sqlService.connect();
+        let results = await sqlService.logoutUser(username, online_uuid);
+        await sqlService.disconnect();
+
+        if(results == true){
+            res.json({success: {status: "USER_LOGGED_OUT", message: "the user has been successfully logged out"}});
+        }
+        else{
+            res.json({success: {status: "WRONG_CREDENTIALS", message: "the username and/or online_uuid are incorrect!"}});
         }
     }
     
