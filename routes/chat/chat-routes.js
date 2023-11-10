e9a0585359af05d1ec57c10e113e4f2016768f83
Červinka <mcervinka17@gmail.com>
2023-11-10 13:05:06 +0100

    added message sending

--- routes/chat/chat-routes.js
+++ routes/chat/chat-routes.js
@@ -12,17 +12,35 @@
 const express = require('express');
 const config = require('config');
 
-const generateUUID = require('../../helpers/uuid-generator');
-const generateHash = require('../../helpers/hash-generator');
 const log = require('../../helpers/logger');
 const sqlService = require('../../model/master/sql-service');
+const Message = require('../../model/core/schemas/messages');
 
 const router = express.Router();
 
 
-router.post('/send', async function(req, res) {
-    
-    
+router.post('/send', async function (req, res) {
+    const requestData = req.body;
+    let from = requestData.from;
+    let to = requestData.to;
+    let content = requestData.content;
+    let online_uuid = requestData.uuid;
+
+    log.info('from: ' + from);
+    log.info('to: ' + to);
+    log.info('content: ' + content);
+
+    if (from == null) {
+        res.json({ error: { status: "SENDER_NOT_SPECIFIED" } });
+    }
+    else if (to == null) {
+        res.json({ error: { status: "RECEIVER_NOT_SPECIFIED" } });
+    }
+
+    await sqlService.connect();
+    await sqlService.checkUserAuthorization(from, online_uuid);
+    await sqlService.disconnect();
+
 });
 
 module.exports = router;
